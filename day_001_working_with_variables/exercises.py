921c0f46096d35eba7d4340cc029c3cee34c838a
BrunoReis-NZ <brunoreis.nz@gmail.com>
2024-11-08 20:07:13 +1300

    day one - exercises one to 10.

--- day_001_working_with_variables/exercises.py
+++ day_001_working_with_variables/exercises.py
@@ -22,3 +22,52 @@ print("Hi" + " " + name + ".")
 color = input("What is your favorite color? ")
 animal = input("What is your favorite animal? ")
 print(f"your favorite color is {color} and your favorite animal is a {animal}")
+
+# 4. Concatenate Strings
+# Write a program that asks for the user's first name and last name,
+# and prints them together as "First Name: [first name], Last Name: [last name]"
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print("First name: " + first_name + " , Last name: " + last_name)
+
+# 5. Basic Variable Assignment
+# Assign a sentence to a variable, then print that variable.
+
+sentence = "this is a sentence stored in a variable"
+print(sentence)
+
+# 6. Using len() Function
+# Ask the user for a word, then print the length of that word.
+
+word = input("Type a word that you want to count the length: ")
+print(f"the length of '{word}' is {len(word)} characters")
+
+# 7. Using 'end' Keyword in print()
+# Print three separate statements on the same line, without spaces between them.
+# Experiment with the end keyword.
+
+print("Hello, ", end="")
+print("world ", end="")
+print("!")
+
+# 8. Using 'sep' Keyword in print()
+# Print a list of items separated by commas, without using concatenation.
+
+print("Python", "C#", "Java", "Javascript", sep=" * ")
+
+# 9. Combine len() with User Input
+# Ask the user for their full name, and print a message telling them how many characters are in it.
+
+full_name = input("What is your full name? ")
+name_without_spaces = full_name.replace(" ", "")
+print(f"Your name has {len(name_without_spaces)} characters.")
+
+# 10. Experimenting with f-Strings and sep
+# Ask the user for three favorite foods and print them out in one line, separated by a hyphen.
+
+food_1 = input("What is your first favorite food? ")
+food_2 = input("What is your second favorite food? ")
+food_3 = input("What is your third favorite food? ")
+print(food_1, food_2, food_3, sep=" - ")
+
