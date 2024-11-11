c1ec9638a8505a67e5bea84d9b7fffb497313e01
BrunoReis-NZ <brunoreis.nz@gmail.com>
2024-11-12 07:56:54 +1300

    day 4 - exercises incomplete

---
+++ day_004_radomisation_and_lists/exercises.py
@@ -0,0 +1,47 @@
+import random
+
+# Random Module Exercises
+
+# print("\n1. Generate a Random Integer")
+#
+# # Use randint() to generate a random integer between 1 and 100. Print the result.
+#
+# rand_int_1 = random.randint(1, 100)
+# print(rand_int_1)
+
+# print("\n2. Generate a Random Float")
+#
+# # Use random() to generate a random float between 0 and 1. Print the result.
+#
+# rand_float_1 = random.random()
+# print(rand_float_1)
+
+# print("\n3. Generate a Random Float within a Range")
+#
+# # Use uniform() to generate a random float between 5.5 and 10.5. Print the result.
+#
+# rand_float_2 = random.uniform(5.5, 10.5)
+# print(rand_float_2
+
+# print("\n4. Random Choice from a List")
+#
+# # Create a list of colors and use random.choice() to pick a random color from the list.
+#
+# color_list = ["blue", "red", "yellow", "green", "pink", "gray"]
+# print(random.choice(color_list))
+
+# print("\n5. Random Choice from a Tuple")
+#
+# # Create a tuple of numbers and use random.choice() to pick a random number from the tuple.
+#
+# number_tuple = (1, 2, 3, 4, 5)
+# print(random.choice(number_tuple))
+
+# print("6. Shuffling a List")
+#
+# # Create a list of numbers, then use random.shuffle() to randomly reorder the list.
+#
+# number_list = [1, 2, 3, 4, 5]
+# print(number_list)
+# random.shuffle(number_list)
+# print(number_list)
